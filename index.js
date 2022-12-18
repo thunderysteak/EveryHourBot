49c130448387a0ef4fef26997b14e6ec490ab1b8
thunderysteak <thunderysteak@live.co.uk>
2022-12-18 21:16:48 +0000

    Use ES6 syntax

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //var schedule = require('node-schedule');
 import defaultExport, * as schedule from 'node-schedule'
-
+import * as child from 'child_process';
 
 console.log('Welcome to EveryHourBot Bot.\nVersion 1.1.0');
 
@@ -8,7 +8,7 @@ const rule = new schedule.RecurrenceRule();
 rule.minute = 0;
 
 const job = schedule.scheduleJob(rule, function(){
-        require('child_process').fork('./maincore.js');
+        child.fork('./maincore.js');
         console.log('\nExecuting main core\n');
 });
 

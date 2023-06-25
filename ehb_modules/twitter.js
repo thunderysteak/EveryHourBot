c2a5b6632e7bb68660738c21e60b6f9f004f2de7
ThunderySteak <thunderysteak@live.co.uk>
2023-06-25 03:01:36 +0100

    We're supporting Twitter again apparently

--- ehb_modules/twitter.js
+++ ehb_modules/twitter.js
@@ -1,18 +1,17 @@
-import Twitter from 'twitter-lite'
-import * as fs from 'node:fs/promises'
 import * as dotenv from 'dotenv'
 import { exit } from 'node:process';
-import * as noPromiseFs from 'node:fs'
+import { TwitterApi } from 'twitter-api-v2';
 
-//Are we in Docker?
-//Checks if env variable is present from Dockerfile
+//If we are in docker, grab variables from the system environment. if not, use .env file.
 if (process.env.ISDOCKER != "true" ) {
     dotenv.config()
 }
+
 //Send to parent that we are ready for execution
 process.send('ready') 
-//Define imagePath in Global Scope for doMastodon func to be able to grab payload from parent
+//Define imagePath in Global Scope for our function to be able to grab payload from parent
 let imagePath 
+
 //Make the message receiving an async function so we can ensure we got the payload
 async function getPayload() {
     return new Promise((resolveFunc) => {
@@ -23,62 +22,49 @@ async function getPayload() {
     });
 }
 
+const doTwitter = async () => {
+    /*
 
+    Hours wasted to untangle the auth clusterfuck: 3 Hours
+    Monster Energy Ultra drank: 3x
 
-    console.log("Doing Twitter");
-    //Create media upload client with different API endpoint
-    //npm library not using correct API endpoint
-    //https://github.com/draftbit/twitter-lite/issues/116
-    const mediaClient = new Twitter({
-        subdomain: "upload",
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token_key: process.env.ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
-    });
+    */
 
-    //Create tweet upload client
-    const client = new Twitter({
-        subdomain: "api",
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token_key: process.env.ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
-    });
+    //Put the tokens into variables or suffer a terrible fate
+    const twitterAppKey = process.env.TWITTER_CUSTOMER_API_KEY
+    const twitterAppKeySecret = process.env.TWITTER_CUSTOMER_API_KEY_SECRET
+    const twitterAccessToken = process.env.TWITTER_ACCESS_TOKEN
+    const twitterAccessTokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET
+    const client = new TwitterApi({
+        appKey: twitterAppKey,
+        appSecret: twitterAppKeySecret,
+        accessToken: twitterAccessToken,
+        accessSecret: twitterAccessTokenSecret,
+    })
+    //
+    const twitterClient = client.readWrite
+    //Just upload the media using V1 API and then publish the Tweet with the uploaded media using V2
+    //Statements dreamed up by the utterly deranged
+    const v1MediaId = await Promise.all([
+        //Don't even need to change it to base64 or load into buffer what is this module
+        twitterClient.v1.uploadMedia(imagePath),
+      ]);
+    /* Somehow hitting the 50 requests/24hr limits 
+    BORN TO DIE
+    TWITTER IS A FUCK
+    Kill Em All 1989
+    I am trash man
+    410,757,864,530 DEAD BOTS
+    */
+    await twitterClient.v2.tweet('', {'media': { media_ids: v1MediaId }},);
+}
 
-    //This whole function might need to be rewritten once twitter-lite has stable V2 API support
-    //http://gitlab.home.thunderysteak.com/thunderysteak/PossumEveryHour/-/issues/2
-    const createTwitterPost = async () => {
-
-        //Read image file data and convert it to Base64 as required by the Twitter API 1.1
-        //https://developer.twitter.com/en/docs/twitter-api/v1/media/upload-media/api-reference/post-media-upload
-        const imageData = await fs.readFile(imagePath) 
-        console.log("Uploading base64 image...")
-        //Send the base64 data to Twitter media API endpoint and get the media ID string
-        const mediaUploadResponse = await mediaClient.post("media/upload", {
-            media_data: imageData.toString("base64"),
-        });
-
-        //Create new tweet with the media ID that was uploaded.
-        //If we wanted to start adding text to posts, this is where we'd do it.
-
-        try {
-            console.log("Creating tweet...")
-            await client.post("statuses/update", { media_ids: mediaUploadResponse.media_id_string })
-        } catch(e) {
-            console.error(`Error creating tweet: ${e}`)
-        }
-    }
-
-    await getPayload()
-        .then(() => {
-            createTwitterPost()
-            .then(() => {
-                console.log("\n\n\nTweet has been successfully sent.\n\n\n");
-                exit(0)
-            }).catch(err => {
-                console.error(err);
-                exit(1)
-            });
-        })
-
+//Use async logic where we first obtain the payload, then perform our function and then exit the process.
+getPayload()
+.then(() => {
+    doTwitter()
+    .then(() => {
+        console.log("Tweet Published")
+        exit(0)
+    })
+})

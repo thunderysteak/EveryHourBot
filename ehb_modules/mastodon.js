3f3614410cb14707bffc1b9edc32c98ca1d4c193
ThunderySteak <thunderysteak@live.co.uk>
2023-07-01 18:54:07 +0100

    Bump compatibility from mastojs 4.x to 5.x

--- ehb_modules/mastodon.js
+++ ehb_modules/mastodon.js
@@ -1,9 +1,8 @@
-//We need to use fs.createReadStream as fs/promise causes unusual errors with Mast.js
-import * as noPromiseFs from 'node:fs'
+import fs from 'node:fs';
 import * as dotenv from 'dotenv'
 import { login } from 'masto';
 import { exit } from 'node:process';
-import { MastoTimeoutError } from "masto"
+
 
 if (process.env.ISDOCKER != "true" ) {
     dotenv.config()
@@ -13,6 +12,9 @@ if (process.env.ISDOCKER != "true" ) {
 process.send('ready') 
 //Define imagePath in Global Scope for doMastodon func to be able to grab payload from parent
 let imagePath 
+
+//Define tootRetryCount in Global Scope so we're not looping infinitely
+let tootRetryCount 
 
 //Make the message receiving an async function so we can ensure we got the payload
 async function getPayload() {
@@ -27,27 +29,32 @@ async function getPayload() {
 const doMastodon = async () => {
     console.log("Doing Mastodon");
 
-
     try {
     const masto = await login({
         url: process.env.MASTODON_SERVER,
         accessToken: process.env.MASTODON_ACCESS_TOKEN,
     });
-    //We need to use fs.createReadStream as fs/promise causes Delayed-Stream module to fail
-    const attachment = await masto.mediaAttachments.create({
-        file: noPromiseFs.createReadStream(imagePath),
+
+    const attachment = await masto.v2.mediaAttachments.create({
+        file: new Blob([fs.readFileSync(imagePath)]),
+        description: '', //Here's where we'd put alt text
     });
-    await masto.statuses.create({
-        status: '',
-        visibility: 'public',
-        mediaIds: [attachment.id],
+
+    const status = await masto.v1.statuses.create({
+    status: '',
+    visibility: 'public',
+    mediaIds: [attachment.id],
     });
+
+    console.log(status);
     //FIXME
     //Need to limit this to not run infinite times and cause issues
     //Uploads to larger instance seem to time out around 3 to 5 times before a successful upload
     } catch(e) {
-        if (String(e).includes("Timeout") || e instanceof MastoTimeoutError) {
+         if (String(e).includes("MastoTimeoutError") || (tootRetryCount <= 3)) {
             console.log("Mastodon timed out. Retrying...")
+            tootRetryCount++
+            console.log("We retried "+tootRetryCount+" times.")
             doMastodon()
         }
         else {
